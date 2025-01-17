546e3682faa3cd23e78d4ec9450ed6771d8edcf3
Brandon Maier <brandon.maier@gmail.com>
2025-01-17 08:46:44 -0600

    port in upstreamable patch

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Add hunspell dictionary paths relative to PREFIX. We have to do this here so
-# that $PREFIX gets expanded into the patch.
-sed "s;\$CONDA_PREFIX;${PREFIX};"\
-    < "${RECIPE_DIR}/0000-dictionary-search-path.patch" \
-    | patch -p1
-
 autoreconf -vfi
 
 ./configure \
